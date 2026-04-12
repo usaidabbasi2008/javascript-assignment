086aa825dd366ae4c9341b746786af59469b7e2f
BilalAbbasi78 <bilalraza4112@gmail.com>
2026-04-12 12:26:39 +0500

    first alerts

---
+++ chapter 1 Alerts/main.js
@@ -0,0 +1,4 @@
+alert("welcome to my website")
+alert("Error ! please enter a valid password")
+alert("welcome to js land... \n  Happy coding !")
+alert("hello... i can run js Through my web browser console")
