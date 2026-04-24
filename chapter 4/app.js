eeb04d2f32df85c8ff726bfb876580450ea8e737
UsaidAbbasi <gmr29292@gmail.com>
2026-04-24 16:07:43 +0500

    varibles names leggal or illegal

---
+++ chapter 4/app.js
@@ -0,0 +1,22 @@
+let name = "   usaid   "
+let cast = "abbasi   "
+let age =   18
+
+alert(name+cast+age )
+
+ let  legal1 = "  _5legalvariblename  "
+ let  legal2= "   $legalvariblenames  "
+ let legal3 = "   legalvariblenames  "
+ let legal4 = "   legal_variblenames  "
+ let legal5 = "   legalvariblenames5  " 
+
+ document.write( "<h4>legal</h4>" +"<br>" + legal1 + legal2 + legal3 + legal4 + legal5 + "<br>")
+
+ let illegal1 = "  5illegalvariblename  "
+ let illegal2 = "   -illegalvariblenames  "
+ let illegal3 = "   .illegalvariblenames  "
+ let illegal4 = "   illegal variblenames  "
+ let illegal5 = "   illegalvariblenames%5  "
+ document.write( "<h4>illegal</h4>" +"<br>" + illegal1 + illegal2 + illegal3 + illegal4 + illegal5 + "<br>")
+
+document.write("<h4>Rules for naming JS variables</h4>" + "<br>" + "Variable names can only contain letters, numbers, underscores, and dollar signs." + "<br>" + "Variables must begin with a letter, underscore, or dollar sign." + "<br>" + "Variable names are case sensitive." + "<br>" + "Variable names should not be JS keywords.")
