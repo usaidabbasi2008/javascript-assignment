6e8521c3991c324a4bc0a233fbaaa92390ded635
UsaidAbbasi <gmr29292@gmail.com>
2026-04-24 14:10:33 +0500

    names and variable

---
+++ chapter 3/main.js
@@ -0,0 +1,15 @@
+let age = 18 
+
+alert ("I am " + age + " years old")
+
+let visitedTimes = 14
+
+alert ("You have visited this site " + visitedTimes + " times")
+
+let birthYear = 2008
+document.write ("My birth year is " + birthYear + "<br>" + "Data type of my declared variable is number" + "<br>")
+
+let visitorName = "John Doe"
+let productTitle = "T-shirt"
+let quantity = 5
+document.write (visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store")
