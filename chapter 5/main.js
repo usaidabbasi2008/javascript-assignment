fc6d1648b141a55ad0ae4ead1e73daf9d50b55bf
UsaidAbbasi <gmr29292@gmail.com>
2026-04-24 16:08:32 +0500

    math expression

---
+++ chapter 5/main.js
@@ -0,0 +1,31 @@
+let sum1 = 49
+let sum2 = 11
+document.write("The sum of " + sum1 + " and " + sum2 + " is " + (sum1 + sum2) + "." + "<br>")
+document.write("the multiplication of " + sum1 + " and " + sum2 + " is " + (sum1 * sum2) + "." + "<br>")
+document.write("the division of " + sum1 + " and " + sum2 + " is " + (sum1 / sum2) + ". " + "<br>")
+document.write("the subtraction of " + sum1 + " and " + sum2 + " is " + (sum1 - sum2) + "." + "<br>"    )
+document.write("the modulus of " + sum1 + " and " + sum2 + " is " + (sum1 % sum2) + ".")
+
+let num1 = 5
+let num2 = 3
+document.write("the value of num1 is " + num1 + "." + "<br>")
+document.write("the value of num2 is " + num2 + "." + "<br>")
+num1++
+document.write("the value of num1 after increment is " + num1 + "." + "<br>")
+num2--
+document.write("the value of num2 after decrement is " + num2 + "." + "<br>")
+
+let totalCost = 600*5
+document.write("the total cost to buy 5 movie tickets is " + totalCost + "." + "<br>")  
+
+document.write("the multiplication table of 4:" + "<br>")
+document.write("4 x 1 = " + (4*1) + "<br>")
+document.write("4 x 2 = " + (4*2) + "<br>")
+document.write("4 x 3 = " + (4*3) + "<br>") 
+document.write("4 x 4 = " + (4*4) + "<br>")
+document.write("4 x 5 = " + (4*5) + "<br> ")
+document.write("4 x 6 = " + (4*6) + "<br>")
+document.write("4 x 7 = " + (4*7) + "<br>")
+document.write("4 x 8 = " + (4*8) + "<br>")
+document.write("4 x 9 = " + (4*9) + "<br>")
+document.write("4 x 10 = " + (4*10) + "<br>")   
